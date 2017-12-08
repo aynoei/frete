c9f4bac332ad355230c6d127956b8b8002a39b52
Your NameDaniel <aynoei@hotmail.com>
2017-12-08 15:32:35 -0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireAuthModule }from 'angularfire2/auth'; 
 
 import { MyApp } from './app.component';
-import { FIREBASE_CONFIG } from "./app.firebase.config";
+import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
 
 @NgModule({
   declarations: [
     MyApp,
+    HomePage,
+    ListPage
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
-    AngularFireModule.initializeApp(FIREBASE_CONFIG),
-    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp
+    MyApp,
+    HomePage,
+    ListPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,33 +1,14 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
-import { AngularFireAuth } from 'angularfire2/auth';
+import { NavController } from 'ionic-angular';
 
-
-@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html',
+  templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(private afAuth: AngularFireAuth, private toast: ToastController,
-    public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController) {
+
   }
 
-  ionViewWillLoad() {
-    this.afAuth.authState.subscribe(data => {
-      if (data && data.email && data.uid) {
-        this.toast.create({
-          message: `Welcome to APP_NAME, ${data.email}`,
-          duration: 3000
-        }).present();
-      }
-      else {
-        this.toast.create({
-          message: `Could not find authentication details.`,
-          duration: 3000
-        }).present();
-      }
-    })
-  }
 }
